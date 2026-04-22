72377d1d05e959a8fcbebfadbf4990f9d4c089c4
uzair <uzair.khan@univr.it>
2026-04-22 15:16:02 +0200

    Refactor main function to include training logic and update argument initialization

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from utils.args import Args
+from pipeline.train import training
 
+def main(args):
+    if args.mode == "train":
+        training(args)
 
 if __name__ == "__main__":
+
+    CLIP_args = {
+        "model_id": "openai/clip-vit-large-patch14-336"
+    }
+
+    generic_args = {
+        "device": "cuda:0",
+        "base_dir": "./",
+        "dataset_name": "MVTec",
+        'mode': "train"
+    }
+
+
     args = Args(
-        name="test",
-        version=1.0,
-        debug=True
+        **CLIP_args,
+        **generic_args
     )
+
+    print(args)
+
+    main(args)
