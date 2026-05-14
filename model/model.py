437ccef7752c34c7510f41e9067d1c38295cc9a2
uzair <uzair.khan@univr.it>
2026-05-14 16:59:30 +0200

    Refactor ZSADModel: streamline attention mechanisms and introduce global top-k ratio for enhanced contrastive learning

--- model/model.py
+++ model/model.py
@@ -15,24 +15,22 @@ class ZSADModel(Module):
                               for _ in range(self.number_of_vision_layers)])
         self.cls_adapter = ModuleList([Adapter(1024, args.out_dim, bottleneck=None, last_activation=True)
                               for _ in range(self.number_of_vision_layers)])
-        self.patch_fusion = Linear(1024 * self.number_of_vision_layers, args.out_dim)
+
+
         self.text_logit_scale = Parameter(torch.ones([]) * torch.log(torch.tensor(1 / 0.07)))
 
         self.cross_model_contrastive_learning_attention = ModuleList([CrossAttention(args.out_dim, 1, 0.2)
                                                                       for _ in range(self.number_of_vision_layers)])
 
-        self.anomaly_aware_calibration_attention = ModuleList([CrossAttention(args.out_dim, 1, 0.2)
-                                                                      for _ in range(self.number_of_vision_layers)])
-
-        self.global_anomaly_alignment_attention = ModuleList([CrossAttention(args.out_dim, 1, 0.2)
-                                                                      for _ in range(self.number_of_vision_layers)])
         self.cross_model_contrastive_head = ModuleList([Linear(args.out_dim, 1)
                                                         for _ in range(self.number_of_vision_layers)])
-        self.anomaly_aware_calibration_head = ModuleList([Linear(args.out_dim, 1)
-                                                          for _ in range(self.number_of_vision_layers)])
-        self.global_anomaly_alignment_head = ModuleList([Linear(args.out_dim, 1)
-                                                         for _ in range(self.number_of_vision_layers)])
 
+        self.layer_logits = Parameter(torch.zeros(self.number_of_vision_layers))
+        self.cls_layer_logits = Parameter(torch.zeros(self.number_of_vision_layers))
+        self.global_patch_layer_logits = Parameter(torch.zeros(self.number_of_vision_layers))
+        self.global_fusion_logits = Parameter(torch.zeros(2))
+        self.global_topk_ratio = getattr(args, "global_topk_ratio", 0.01)
+        
         self.img_size = args.img_size
 
     def normalize_features(self, features):
@@ -55,66 +53,73 @@ class ZSADModel(Module):
     def merge_text_embeddings(self, normal_text_adapted, abnormal_text_adapted):
         return torch.cat([normal_text_adapted, abnormal_text_adapted], dim=1)
 
-    def patch_global_embedding(self, patch_features):
-        batch_size, layer_count, patch_count, channels = patch_features.shape
-        patch_features = patch_features.transpose(1, 2).contiguous()
-        patch_features = patch_features.view(batch_size, patch_count, layer_count * channels)
-        patch_features = patch_features.mean(dim=1)
-        return self.patch_fusion(patch_features)
-
-    def text_branch_anomaly_logits(
-        self,
-        normal_text_adapted,
-        abnormal_text_adapted,
-        cls_embeddings_adapted,
-        patch_features,
-    ):
-        x_cls = cls_embeddings_adapted[-1]
-        x_patch = self.normalize_features(self.patch_global_embedding(patch_features))
-        image_embedding = self.normalize_features(x_cls + x_patch)
-
-        logit_scale = self.text_logit_scale.exp().clamp(max=100)
-        normal_logits = logit_scale * torch.einsum(
-            'bd,bnd->bn',
-            image_embedding,
-            normal_text_adapted,
-        )
-        abnormal_logits = logit_scale * torch.einsum(
-            'bd,bnd->bn',
-            image_embedding,
-            abnormal_text_adapted,
-        )
-        normal_score = normal_logits.mean(dim=1, keepdim=True)
-        abnormal_score = abnormal_logits.mean(dim=1, keepdim=True)
-        return abnormal_score - normal_score
-
     def cross_model_contrastive_learning(self, text_embeddings_adapted, patch_embeddings_adapted, layer_idx):
         patch_features = self.cross_model_contrastive_learning_attention[layer_idx](
             patch_embeddings_adapted,
             text_embeddings_adapted,
         )
-        anomaly_map_cross_modal = self.cross_model_contrastive_head[layer_idx](patch_features)
-        anomaly_map_cross_modal = F.interpolate(self.patch_logits_to_map(anomaly_map_cross_modal),
+        cross_modal_patch_logits = self.cross_model_contrastive_head[layer_idx](patch_features)
+        anomaly_map_cross_modal = F.interpolate(self.patch_logits_to_map(cross_modal_patch_logits),
                                     size=self.img_size, mode='bilinear', align_corners=True)
-        return anomaly_map_cross_modal
+        return anomaly_map_cross_modal, cross_modal_patch_logits
 
-    def anomaly_aware_calibration(self, adapted_patch_features, cls_embeddings_adapted, layer_idx):
-        anomaly_awareness_features = self.anomaly_aware_calibration_attention[layer_idx](
-            adapted_patch_features,
-            cls_embeddings_adapted,
+    def text_contrast_logits(self, image_embeddings, normal_text_adapted, abnormal_text_adapted):
+        image_embeddings = self.normalize_features(image_embeddings)
+        logit_scale = self.text_logit_scale.exp().clamp(max=100)
+        normal_logits = torch.einsum(
+            'bd,bnd->bn',
+            image_embeddings,
+            normal_text_adapted,
         )
-        anomaly_awareness_cls_patch = self.anomaly_aware_calibration_head[layer_idx](anomaly_awareness_features)
-        anomaly_awareness_cls_patch = F.interpolate(self.patch_logits_to_map(anomaly_awareness_cls_patch),
-                                    size=self.img_size, mode='bilinear', align_corners=True)
-        return anomaly_awareness_cls_patch
-    
+        abnormal_logits = torch.einsum(
+            'bd,bnd->bn',
+            image_embeddings,
+            abnormal_text_adapted,
+        )
+        normal_score = normal_logits.mean(dim=1, keepdim=True)
+        abnormal_score = abnormal_logits.mean(dim=1, keepdim=True)
+        return logit_scale * (abnormal_score - normal_score)
 
-    def global_anomaly_allignment(self, cls_embeddings_adapted, text_embeddings_adapted, layer_idx):
-        cls_features = self.global_anomaly_alignment_attention[layer_idx](
+    def cls_text_contrast_logits(self, cls_embeddings_adapted, normal_text_adapted, abnormal_text_adapted):
+        layer_logits = torch.stack([
+            self.text_contrast_logits(cls_embeddings_adapted[i], normal_text_adapted, abnormal_text_adapted)
+            for i in range(self.number_of_vision_layers)
+        ], dim=1)
+        layer_weights = torch.softmax(self.cls_layer_logits, dim=0)
+        return (layer_logits * layer_weights.view(1, -1, 1)).sum(dim=1)
+
+    def topk_cross_modal_logits(self, cross_modal_patch_logits):
+        layer_logits = []
+
+        for patch_logits in cross_modal_patch_logits:
+            patch_logits = patch_logits.squeeze(-1)
+            patch_count = patch_logits.shape[1]
+            topk = max(1, int(patch_count * self.global_topk_ratio))
+            layer_logits.append(patch_logits.topk(topk, dim=1).values.mean(dim=1, keepdim=True).detach())
+
+        layer_logits = torch.stack(layer_logits, dim=1)
+        layer_weights = torch.softmax(self.global_patch_layer_logits, dim=0)
+        return (layer_logits * layer_weights.view(1, -1, 1)).sum(dim=1)
+
+    def global_text_contrast_logits(
+        self,
+        cls_embeddings_adapted,
+        cross_modal_patch_logits,
+        normal_text_adapted,
+        abnormal_text_adapted,
+    ):
+        # Stop the global CLS/text loss from training text adapters,
+        # while keeping gradients into the CLS adapters.
+        cls_normal_text_adapted = normal_text_adapted.detach()
+        cls_abnormal_text_adapted = abnormal_text_adapted.detach()
+        cls_logits = self.cls_text_contrast_logits(
             cls_embeddings_adapted,
-            text_embeddings_adapted,
+            cls_normal_text_adapted,
+            cls_abnormal_text_adapted,
         )
-        return self.global_anomaly_alignment_head[layer_idx](cls_features).squeeze(1)
+        patch_logits = self.topk_cross_modal_logits(cross_modal_patch_logits)
+        fusion_weights = torch.softmax(self.global_fusion_logits, dim=0)
+        return 1 * cls_logits + 1 * patch_logits
     
     def forward(self, text_embeddings, image_features):
         normal_text_embeddings, abnormal_text_embeddings = self.unpack_text_embeddings(text_embeddings)
@@ -136,19 +141,26 @@ class ZSADModel(Module):
             patch_embedding_adapted = self.normalize_features(patch_embedding_adapted)
             patch_embeddings_adapted.append(patch_embedding_adapted)
 
-        cross_model_contrastive = torch.stack([
-            self.cross_model_contrastive_learning(adapted_text_embeddings, patch_embeddings_adapted[i], i)
-            for i in range(self.number_of_vision_layers)], dim=1)
+        cross_model_contrastive = []
+        cross_modal_patch_logits = []
+        for i in range(self.number_of_vision_layers):
+            anomaly_map_cross_modal, patch_logits = self.cross_model_contrastive_learning(
+                adapted_text_embeddings,
+                patch_embeddings_adapted[i],
+                i,
+            )
+            cross_model_contrastive.append(anomaly_map_cross_modal)
+            cross_modal_patch_logits.append(patch_logits)
+        cross_model_contrastive = torch.stack(cross_model_contrastive, dim=1)
 
-        # anomaly_aware_calibration = torch.stack([
-        #     self.anomaly_aware_calibration(patch_embeddings_adapted[i], cls_embeddings_adapted[i].unsqueeze(1), i)
-        #     for i in range(self.number_of_vision_layers)], dim=1)
-
-        global_anomaly_alignment = self.text_branch_anomaly_logits(
+        global_anomaly_alignment = self.global_text_contrast_logits(
+            cls_embeddings_adapted,
+            cross_modal_patch_logits,
             positive_text_adapted,
             negative_text_adapted,
-            cls_embeddings_adapted,
-            image_features[1],
         )
 
-        return cross_model_contrastive.mean(dim=1), global_anomaly_alignment
+        weights = torch.softmax(self.layer_logits, dim=0)
+        final_map = (cross_model_contrastive * weights.view(1, -1, 1, 1, 1)).sum(dim=1)
+
+        return final_map, global_anomaly_alignment
